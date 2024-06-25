8f339c5cbdd5c62c8255923ed177f612430f53a8
ibm <butshikan@gmail.com>
2024-06-26 00:30:20 +0500

    deployment build

---
+++ dist/assets/decimals-AL1O_QbM.js
@@ -0,0 +1,1 @@
+import{bI as a}from"./index-Cq-SXoro.js";import{d as e}from"./decimals-Dl13iPTw.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
--- dist/assets/decimals-RNJL0Frg.js
+++
@@ -1,1 +0,0 @@
-import{bI as a}from"./index-xhjb5zK7.js";import{d as e}from"./decimals-BCWpbT6W.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
