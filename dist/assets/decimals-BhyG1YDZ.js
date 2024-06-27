3e19c7367ee0275c475dac156891c630e0d67e91
komalashrafsyed <komalashrafsyed@users.noreply.github.com>
2024-06-27 17:36:16 +0500

    Attempt 5

--- dist/assets/decimals-BhyG1YDZ.js
+++
@@ -1,1 +0,0 @@
-import{bI as a}from"./index-Cxf8tBRh.js";import{d as e}from"./decimals-Ch6dnhq7.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
