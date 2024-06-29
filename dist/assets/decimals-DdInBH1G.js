8b460eb7d24c14565c3c9631dd68ea04a2dfad0f
komalashrafsyed <komalashrafsyed@users.noreply.github.com>
2024-06-30 00:24:50 +0500

    demo push

--- dist/assets/decimals-DdInBH1G.js
+++
@@ -1,1 +0,0 @@
-import{bJ as a}from"./index-ClU_xU87.js";import{d as e}from"./decimals-QvbfSTvX.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
