3b55bce4d8d6f6882c2004ec8f08e0af9ca85261
komalashrafsyed <komalashrafsyed@users.noreply.github.com>
2024-06-30 17:09:02 +0500

    demo final 3rd last push

--- dist/assets/decimals-vsPWuiEh.js
+++
@@ -1,1 +0,0 @@
-import{bJ as a}from"./index-Wh4134Ya.js";import{d as e}from"./decimals-B_6358vx.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
---
+++ dist/assets/eth_estimateGas-DEghfB_L.js
@@ -0,0 +1,1 @@
+import{w as s}from"./index-CZOWGhN9.js";async function m(t,e){const a=await t({method:"eth_estimateGas",params:[e]});return s(a)}export{m as eth_estimateGas};
