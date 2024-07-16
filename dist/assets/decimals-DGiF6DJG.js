985bcb4a217a8597db8a8ebdd0ac8fd96e9b4e5b
komalashrafsyed <komalashrafsyed@users.noreply.github.com>
2024-07-17 02:28:15 +0500

    pushing lastest

---
+++ dist/assets/decimals-DGiF6DJG.js
@@ -0,0 +1,1 @@
+import{bJ as a}from"./index-CqkGq1z7.js";import{d as e}from"./decimals-kTLcgydc.js";async function i(c){return a(()=>e(c),{cacheKey:`${c.contract.chain.id}:${c.contract.address}:decimals`,cacheTime:Number.POSITIVE_INFINITY})}export{i as decimals};
